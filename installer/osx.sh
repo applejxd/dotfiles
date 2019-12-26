d356cdc1efd0bf2cda2c2a3752d2f8462c93f012
applejxd <break.r.evo@gmail.com>
2019-12-26 21:50:47 +0900

    added command for installing shells

--- installer/osx.sh
+++ installer/osx.sh
@@ -4,11 +4,23 @@
 pip3 install --upgrade setuptools
 pip3 install matplotlib scipy sympy ipython[all]
 
+# bash install
+grep -q /usr/local/bin/bash /etc/shells
+if [ $? -ne 0 ]; then
+    sudo sh -c "echo '/usr/local/bin/bash\n' >> /etc/shells"
+fi
+
 # zsh install
 grep -q /usr/local/bin/zsh /etc/shells
 if [ $? -ne 0 ]; then
     sudo sh -c "echo '/usr/local/bin/zsh\n' >> /etc/shells"
     chsh -s /usr/local/bin/zsh
+fi
+
+# fish install
+grep -q /usr/local/bin/fish /etc/shells
+if [ $? -ne 0 ]; then
+    sudo sh -c "echo '/usr/local/bin/fish\n' >> /etc/shells"
 fi
 
 # fzf install
