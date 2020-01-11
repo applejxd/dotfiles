a9ea91a0b44e98a4c453a8aab34fd52463c97607
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-01-11 14:11:23 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
+source .bashrc
 homeshick clone applejxd/dotfiles
 homeshick link dotfiles
 
