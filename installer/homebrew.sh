b44a1205ff4fda3201f07e814ef992f1a50f905a
applejxd <break.r.evo@gmail.com>
2020-01-11 15:40:22 +0900

    made if sentence to distinguish OS

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if !(type "brew" > /dev/null 2>&1); then
+if [[ "$OSTYPE" == "darwin"* ]]; then
     xcode-select --install
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # for brew doctor
+    sudo chown -R $(whoami) /usr/local/share/man/man5
 fi
-
-# brew doctor
-sudo chown -R $(whoami) /usr/local/share/man/man5
