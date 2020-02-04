883e1a44292febeaef056bdebf7cbbe5102efa82
applejxd <break.r.evo@gmail.com>
2020-02-04 17:20:08 +0900

    added comments

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -7,7 +7,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo chown -R $(whoami) /usr/local/share/man/man5
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    # for adding PATH in a configuration file
     echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
+    # for adding PATH in the active shell
     eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 fi
 
