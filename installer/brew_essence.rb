bfcf8a5228ecede43ad46248f51aa1b4cab7be29
applejxd <break.r.evo@gmail.com>
2020-04-18 14:13:35 +0900

    moved common applications from osx to essence

--- installer/brew_essence.rb
+++ installer/brew_essence.rb
@@ -48,3 +48,48 @@ brew "wget"
 brew "tree"
 brew "ispell"
 brew "nkf"
+
+#--------------#
+# Developments #
+#--------------#
+
+# Python
+brew "python3"
+brew "python@2"
+brew "pipenv"
+
+# C++
+brew "boost"
+brew "opencv"
+
+#---------#
+# Science #
+#---------#
+
+# Cadabra2
+tap "kpeeters/repo"
+brew "cadabra2"
+
+# WxMaxima
+brew "gnuplot"
+brew "maxima"
+brew "wxmaxima"
+
+# Octave
+brew "octave"
+
+#----------------#
+# Entertainments #
+#----------------#
+
+brew "youtube-dl"
+brew "ffmpeg"
+brew "mediainfo"
+
+# show this environment
+brew "screenfetch"
+
+# 元祖 rogue 日本語版
+# https://leopard-gecko.github.io/jrogue/
+tap "leopard-gecko/game"
+brew "jrogue"
--- installer/brew_osx.rb
+++ installer/brew_osx.rb
@@ -6,51 +6,6 @@ if OS.mac?
     brew "m-cli"
 end
 
-#--------------#
-# Developments #
-#--------------#
-
-# Python
-brew "python3"
-brew "python@2"
-brew "pipenv"
-
-# C++
-brew "boost"
-brew "opencv"
-
-#---------#
-# Science #
-#---------#
-
-# Cadabra2
-tap "kpeeters/repo"
-brew "cadabra2"
-
-# WxMaxima
-brew "gnuplot"
-brew "maxima"
-brew "wxmaxima"
-
-# Octave
-brew "octave"
-
-#----------------#
-# Entertainments #
-#----------------#
-
-brew "youtube-dl"
-brew "ffmpeg"
-brew "mediainfo"
-
-# show this environment
-brew "screenfetch"
-
-# 元祖 rogue 日本語版
-# https://leopard-gecko.github.io/jrogue/
-tap "leopard-gecko/game"
-brew "jrogue"
-
 #################
 # Mac App Store #
 #################
