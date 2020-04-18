d3e51d35c1c3a92609f42debbdf1e3e87826b662
applejxd <break.r.evo@gmail.com>
2020-04-18 23:32:27 +0900

    added xfce4 for GUI in wsl

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -17,3 +17,8 @@ sudo apt install -y cadabra
 
 # Emacs keybind
 gsettings set org.gnome.desktop.interface gtk-key-theme Emacs
+
+# GUI in WSL
+if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+    sudo apt install -y xfce4-terminal xfce4
+fi
