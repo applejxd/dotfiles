415676942f26d3ae801adf387418d15f52c436a9
applejxd <myamazaki.phys@gmail.com>
2020-05-03 10:05:59 +0900

    sepalate misc from brew_essence.rb

--- installer/brew_essence.rb
+++ installer/brew_essence.rb
@@ -57,35 +57,3 @@ brew "pipenv"
 # C++
 brew "boost"
 brew "opencv"
-
-#---------#
-# Science #
-#---------#
-
-# Cadabra2
-tap "kpeeters/repo"
-brew "cadabra2"
-
-# WxMaxima
-brew "gnuplot"
-brew "maxima"
-brew "wxmaxima"
-
-# Octave
-brew "octave"
-
-#----------------#
-# Entertainments #
-#----------------#
-
-brew "youtube-dl"
-brew "ffmpeg"
-brew "mediainfo"
-
-# show this environment
-brew "screenfetch"
-
-# 元祖 rogue 日本語版
-# https://leopard-gecko.github.io/jrogue/
-tap "leopard-gecko/game"
-brew "jrogue"
