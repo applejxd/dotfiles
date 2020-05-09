fc505a5fd7a7184b59ced83eabe2dab36aa04000
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-05-09 12:28:50 +0900

    changed comments

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -16,10 +16,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # for brew doctor
     sudo chown -R $(whoami) /usr/local/share/man/man5
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    # for adding PATH in the active shell
+    # cf. http://tinyurl.com/y5yh2vm3
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    # for adding PATH in a configuration file
     test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
     echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 fi
