39b897f882deb04136acea83b425744ff851cf96
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-05-09 23:28:43 +0900

    changed not to use .bashrc

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ fi
 # cf. http://tinyurl.com/yd8kcbq6
 if !(type "gem" > /dev/null 2>&1); then
     brew install rbenv
-    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-    source ~/.bashrc
+    eval "$(rbenv init -)"
     # install default version
     rbenv install 2.5.0
     rbenv rehash
