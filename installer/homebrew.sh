22c7c507a0a04b7aa9ccfaef682cb6442ef216e7
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-05-09 17:03:34 +0900

    added install command for ruby-build

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -6,7 +6,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif [[ -e /etc/lsb-release ]]; then
     sudo apt update -y
     sudo apt upgrade -y
+    # for Homebrew (cf. http://tinyurl.com/y5yh2vm3)
     sudo apt install -y build-essential curl file git
+    # for ruby-build (cf. http://tinyurl.com/j6g67up)
+    sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 fi
 
 # Install Homebrew for Mac OS X or Linux
@@ -16,7 +19,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # for brew doctor
     sudo chown -R $(whoami) /usr/local/share/man/man5
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    # cf. http://tinyurl.com/y5yh2vm3
+    # for Homebrew (cf. http://tinyurl.com/y5yh2vm3)
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
