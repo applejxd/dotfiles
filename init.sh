188969c85731edfecb57e1f7a673b4b413cba258
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-05-11 04:28:20 +0900

    prefer Ubuntu apt

--- init.sh
+++ init.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+# for Ubuntu (cf. http://bit.ly/37WjcWG)
+if [[ -e /etc/lsb-release ]]; then
+    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/ubuntu.sh)
+fi
+
 if !(type "brew" > /dev/null 2>&1); then
     source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/homebrew.sh)
 fi
 
 brew bundle --file=<(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/brew_essence.rb) 2>/dev/null
 
-# cf. http://bit.ly/37WjcWG
+# for Mac OS X
 if [[ "$OSTYPE" == "darwin"* ]]; then
     brew bundle --file=<(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/brew_osx.rb) 2>/dev/null
     source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/osx.sh)
-elif [[ -e /etc/lsb-release ]]; then
-    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/ubuntu.sh)
 fi
 
 source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/shells.sh)
