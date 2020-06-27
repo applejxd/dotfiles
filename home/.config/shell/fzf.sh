f820d1cc50f037801be2b78edf8c179cae024364
applejxd <break.r.evo@gmail.com>
2020-06-28 02:31:45 +0900

    use fzf instead of fzf-tmux for v command

--- home/.config/shell/fzf.sh
+++ home/.config/shell/fzf.sh
@@ -44,7 +44,7 @@ v() {
     files=$(grep '^>' ~/.viminfo | cut -c3- |
         while read line; do
             [ -f "${line/\~/$HOME}" ] && echo "$line"
-        done | fzf-tmux -d -m -q "$*" -1) && vim ${files//\~/$HOME}
+        done | fzf -d -m -q "$*" -1) && vim ${files//\~/$HOME}
 }
 
 # cf. http://bit.ly/37GNSLZ
