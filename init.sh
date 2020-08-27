c47ec293ec60b81a5ec5d8f341c8e67e72795f01
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-08-27 23:49:20 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,14 +5,12 @@ if [[ -e /etc/lsb-release ]]; then
     source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/ubuntu.sh)
 fi
 
-if !(type "brew" > /dev/null 2>&1); then
-    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/homebrew.sh)
-fi
-
-brew bundle --file=<(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/brew_essence.rb) 2>/dev/null
-
 # for Mac OS X
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    if !(type "brew" > /dev/null 2>&1); then
+    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/homebrew.sh)
+    fi
+    brew bundle --file=<(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/brew_essence.rb) 2>/dev/null
     brew bundle --file=<(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/brew_osx.rb) 2>/dev/null
     source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/osx.sh)
 fi
