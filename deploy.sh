3e9018bbc050321257e2f43d25c42016e42370e3
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-08-27 23:22:55 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-if !(type "brew" > /dev/null 2>&1); then
-    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/homebrew.sh)
-fi
-
 # for ruby-build in Ubuntu (cf. http://tinyurl.com/j6g67up)
 if [[ -e /etc/lsb-release ]] && !(type "ruby-build" > /dev/null 2>&1); then
+    sudo apt update
+    sudo apt upgrade -y
     sudo apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev libdb-dev
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]] && !(type "brew" > /dev/null 2>&1); then
+    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/master/installer/homebrew.sh)
 fi
 
 # install rbenv (cf. http://tinyurl.com/yd8kcbq6)
 if !(type "gem" > /dev/null 2>&1); then
     # install anyenv
-    brew install anyenv
-    echo 'eval "$(anyenv init -)"' >> ~/.bashrc
-    anyenv install --init
+    if [[ -e /etc/lsb-release ]]; then
+        git clone https://github.com/anyenv/anyenv ~/.anyenv
+        ~/.anyenv/bin/anyenv init
+        export PATH="$HOME/.anyenv/bin:$PATH"
+        anyenv install --init
+    fi
+    
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install anyenv
+        echo 'eval "$(anyenv init -)"' >> ~/.bashrc
+        anyenv install --init
+    fi
     
     # install rbenv
     anyenv install rbenv
