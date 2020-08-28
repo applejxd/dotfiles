5f73ff754c0e7e3f013362796e6be82beb2d47ec
りんごくん <9639592+applejxd@users.noreply.github.com>
2020-08-28 20:23:14 +0900

    Update ubuntu.sh

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
-# repository (emacs)
-sudo add-apt-repository ppa:kelleyk/emacs
-
 # refresh
 sudo apt -y update
 sudo apt -y upgrade
 
 # basics
-sudo apt install -y manpages-ja emacs26 xsel
+sudo apt install -y manpages-ja
+
+# clipboard
+sudo apt install -y xsel
 
 # Developing
 sudo apt install -y zsh
 
 # LaTeX
-# sudo apt install -y texlive texlive-lang-japanese texlive-xetex texlive-science
 sudo apt install -y texlive-full
 
-# Calc
-sudo apt install -y cadabra
-
-# Emacs keybind
-gsettings set org.gnome.desktop.interface gtk-key-theme Emacs
-
-# GUI in WSL
 if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+    # GUI in WSL
     sudo apt install -y xfce4-terminal xfce4
 else
     sudo apt install -y chromium-browser  
