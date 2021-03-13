bb4c5abd838bf90be62bcf5d5a015cc7b6721db6
applejxd <9639592+applejxd@users.noreply.github.com>
2021-03-13 16:23:30 +0900

    update script

--- installer/jetbrains.sh
+++ installer/jetbrains.sh
@@ -8,10 +8,15 @@ else
     password=$1
 fi
 
+# wsl.conf の設定（シンボリックリンクでなければ実行）
+if [ ! -L /etc/wsl.conf ]; then
+    echo "$password" | sudo -S rm /etc/wsl.conf
+    echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/wsl.conf /etc/wsl.conf
+fi
+
 if !(id jetbrains >/dev/null 2>&1); then
     echo "$password" | sudo -S useradd -s /bin/bash -m jetbrains
     echo "$password" | sudo -S passwd --stdin jetbrains
     echo "export DISPLAY=\$(cat /etc/resolv.conf | grep nameserver | awk '{print \$2}'):0.0" > /home/jetbrains/shellenv.sh
     echo "$password" | sudo -S -u jetbrains wget https://raw.githubusercontent.com/JetBrains/clion-wsl/master/ubuntu_setup_env.sh && bash ubuntu_setup_env.sh
-
 fi
