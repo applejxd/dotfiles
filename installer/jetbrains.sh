807bc92520fccef28c0b51826e8ddae23da63bd0
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-03-21 21:58:15 +0900

    Update jetbrains.sh

--- installer/jetbrains.sh
+++ installer/jetbrains.sh
@@ -15,7 +15,7 @@ fi
 
 if !(id jetbrains >/dev/null 2>&1); then
     echo "$password" | sudo -S useradd -s /bin/bash -m jetbrains
-    echo "$password" | sudo -S passwd --stdin jetbrains
+    echo "$password" | passwd --stdin jetbrains
     echo "export DISPLAY=\$(cat /etc/resolv.conf | grep nameserver | awk '{print \$2}'):0.0" > /home/jetbrains/shellenv.sh
     echo "$password" | sudo -S -u jetbrains wget https://raw.githubusercontent.com/JetBrains/clion-wsl/master/ubuntu_setup_env.sh && bash ubuntu_setup_env.sh
 fi
