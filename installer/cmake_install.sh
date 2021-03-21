a59ba98e3f3da668f787cf27c83b281afd596cdc
applejxd <9639592+applejxd@users.noreply.github.com>
2021-03-21 13:56:39 +0900

    add manual install script

---
+++ installer/cmake_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    # save password
+    printf "password: "
+    read password
+else
+    password=$1
+fi
+
+if [ ! -e ~/install ]; then
+  mkdir ~/install
+fi
+
+function cmake_install() {
+  if [ ! -e ~/install/$2/build ]; then
+    git clone $1 ~/install/$2
+    mkdir ~/install/$2/build
+    cd ~/install/$2/build
+    cmake ..
+    make
+    # echo "$password" | sudo -S make install
+  fi
+}
+
+cmake_install https://github.com/google/googletest.git googletest
