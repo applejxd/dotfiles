288cc512cae5c7cfc32e9321973b2ed3c64cfa3c
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-03-22 23:12:43 +0900

    Update develop.sh

--- installer/develop.sh
+++ installer/develop.sh
@@ -7,16 +7,31 @@ else
     password=$1
 fi
 
-# C++
-echo "$password" | sudo -S apt install -y clang-format cpplint doxygen
-echo "$password" | sudo -S apt install -y libboost-dev libeigen3-dev libceres-dev
-echo "$password" | sudo -S apt install -y libboost-dev libopencv-dev libpcl-dev
+#######
+# C++ #
+#######
 
-# Python
+# Formatter, Linter, Document Generator
+echo "$password" | sudo -S apt install -y clang-format cpplint doxygen
+# Boost, Eigen3 (Linear Algebra), Ceres (Optimization)
+echo "$password" | sudo -S apt install -y libboost-dev libeigen3-dev libceres-dev
+# OpenCV (Computer Vision), Point Cloud Library
+echo "$password" | sudo -S apt install -y libopencv-dev libpcl-dev
+# Google Tests
+echo $password | source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/main/installer/cmake_install.sh)
+
+##########
+# Python #
+##########
+
 echo "$password" | sudo -S apt install -y python3-pip 
 pip3 install pipenv
+# Sphinx (Document Generator)
 pip3 install sphinx
 pip3 install sphinx_rtd_theme
 
-# TeX
+#######
+# TeX #
+#######
+
 echo "$password" | sudo -S apt install -y texlive-full
