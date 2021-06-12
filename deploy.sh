e7dbc9d16cf99309a1f751429c4ab77ec8dbd3ce
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-06-12 22:18:05 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ if [[ -e /etc/lsb-release ]] && !(type "ruby-build" > /dev/null 2>&1); then
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]] && !(type "brew" > /dev/null 2>&1); then
-    source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/main/installer/homebrew.sh)
+    # The altanative of process substitution for bash 3.2 that is installed to Mac OS X
+    source /dev/stdin <<<"$( curl -sS https://raw.githubusercontent.com/applejxd/dotfiles/main/installer/homebrew.sh )"
 fi
 
 # install rbenv (cf. http://tinyurl.com/yd8kcbq6)
