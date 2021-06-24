86b532148b091996e53dfef75d45cd4da0078ad5
applejxd <9639592+applejxd@users.noreply.github.com>
2021-06-24 22:18:51 +0900

    optimize

--- installer/osx.sh
+++ installer/osx.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Python
-pip3 install --upgrade setuptools
-pip3 install matplotlib scipy sympy ipython[all]
-
 # fzf install
 if [ ! -e ~/.fzf.zsh ]; then
     $(brew --prefix)/opt/fzf/install
@@ -15,24 +11,18 @@ if [ ! -e ~/.iterm2_shell_integration.zsh ]; then
 fi
 
 # LaTeX
-if !(type "platex" > /dev/null 2>&1); then
+if !(type "platex" > /dev/null 2>&1) && (type tlmgr > /dev/null 2>&1); then
     sudo tlmgr update --self --all
     sudo tlmgr paper a4
 fi
 
-# Programming Font Ricty
-if [ ! -e ~/Library/fonts/Ricty\ Discord\ Regular\ for\ Powerline.ttf ]; then
-    cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-    fc-cache -vf
-fi
+# # Programming Font Ricty
+# if [ ! -e ~/Library/fonts/Ricty\ Discord\ Regular\ for\ Powerline.ttf ]; then
+#     cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+#     fc-cache -vf
+# fi
 
-# powerline, which needs powerline-font
-if !(type "powerline-daemon" > /dev/null 2>&1); then
-    pip3 install powerline-status
-fi
-
-# spacemacs install
-if [ ! -e ~/.emacs.d ]; then
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-    npm install --global tern
-fi
+# # powerline, which needs powerline-font
+# if !(type "powerline-daemon" > /dev/null 2>&1); then
+#     pip3 install powerline-status
+# fi
