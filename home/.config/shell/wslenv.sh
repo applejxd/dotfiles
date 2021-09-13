4ea90fadca48c648143bedf848fd5f2705b4c469
applejxd <9639592+applejxd@users.noreply.github.com>
2021-09-13 19:55:26 +0900

    fix bug

--- home/.config/shell/wslenv.sh
+++ home/.config/shell/wslenv.sh
@@ -3,12 +3,11 @@ export LIBGL_ALWAYS_INDIRECT=1
 
 # for VcXsrv
 if [[ -e /etc/resolv.conf ]]; then
-        # for WSL2
-        export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
-    else
-        # for WSL1
-        export DISPLAY=:0.0
-    fi
+    # for WSL2
+    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+else
+    # for WSL1
+    export DISPLAY=:0.0
 fi
 
 # for JDK (Adoptium)
