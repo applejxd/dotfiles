e86cf871f1e6b2ebc5f24e227c8c0c610636be98
applejxd <9639592+applejxd@users.noreply.github.com>
2021-12-10 23:28:40 +0900

    update

--- home/.config/shell/fzf.sh
+++ home/.config/shell/fzf.sh
@@ -155,25 +155,25 @@ fadd() {
 # Select a docker container to start and attach to
 function da() {
     local cid
-    cid=$(docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}')
+    cid=$(sudo docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}')
 
-    [ -n "$cid" ] && docker start "$cid" && docker attach "$cid"
+    [ -n "$cid" ] && sudo docker start "$cid" && sudo docker attach "$cid"
 }
 
 # Select a running docker container to stop
 function ds() {
     local cid
-    cid=$(docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
+    cid=$(sudo docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
 
-    [ -n "$cid" ] && docker stop "$cid"
+    [ -n "$cid" ] && sudo docker stop "$cid"
 }
 
 # Select a docker container to remove
 function drm() {
     local cid
-    cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
+    cid=$(sudo docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
 
-    [ -n "$cid" ] && docker rm "$cid"
+    [ -n "$cid" ] && sudo docker rm "$cid"
 }
 
 ############
