48037d536e9619e8e05977cd4260eff679c362ab
applejxd <9639592+applejxd@users.noreply.github.com>
2021-12-13 22:52:17 +0900

    remove jetbrains config

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -40,9 +40,6 @@ if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
         echo "$password" | sudo -S rm /etc/wsl.conf
         echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/wsl.conf /etc/wsl.conf
     fi
-    
-    # for JetBrains IDEs
-    echo "$password" | source <(curl -L https://raw.githubusercontent.com/applejxd/dotfiles/main/installer/jetbrains.sh)
 else
     echo "$password" | sudo -S apt install -y chromium-browser  
 fi
