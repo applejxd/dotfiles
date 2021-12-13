72151526d120f6d35bea33994ba53211d359bf56
りんごくん <applejxd@users.noreply.github.com>
2021-12-13 14:30:06 +0900

    update

--- home/.config/shell/wslenv.sh
+++ home/.config/shell/wslenv.sh
@@ -2,7 +2,7 @@
 export LIBGL_ALWAYS_INDIRECT=1
 
 # for VcXsrv
-if [[ -e /etc/resolv.conf ]]; then
+if [[ -d /run/WSL ]]; then
     # for WSL2
     export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
 else
