8065e256344496b2b537fa65bdadd5cc90aafc43
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-02-20 20:07:22 +0900

    Update ubuntu.sh

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -29,9 +29,6 @@ echo "$password" | sudo -S apt-get install -y xsel
 # Filer
 echo "$password" | sudo -S apt-get install -y xdg-utils
 
-# RDP
-echo "$password" | sudo -S apt-get install -y xrdp
-
 # GUI
 # echo "$password" | sudo -S apt-get install -y ubuntu-desktop
 echo "$password" | sudo -S apt-get install -y xubuntu-desktop
@@ -65,38 +62,56 @@ export VERSION=3.9.5 && \
 # echo "$password" | sudo -S apt-get install -y texlive-full
 
 if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
-    # SSH settings
-    echo "$password" | sudo -S apt-get purge openssh-server
-    echo "$password" | sudo -S apt-get install openssh-server
-    if [ ! -L /etc/ssh/sshd_config ]; then
-        echo "$password" | sudo -S rm /etc/ssh/sshd_config
-        echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/sshd_config /etc/ssh/sshd_config
-    fi
-
-    # RDP settings
-    # cf. https://qiita.com/atomyah/items/887a5185ec9a8206c7c4#ubuntu%E3%81%ABxrdp%E3%82%92%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB
-    echo "$password" | sudo -S bash -c "\
-        apt-get -y install xfce4 xrdp && \
-        sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini && \
-        sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini && \
-        sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini"
-    echo xfce4-session > ~/.xsession
-    echo "$password" | sudo -S sed -i 's|^\(test\s-x\s/etc/X11/Xsession.*\)|# \1|' /etc/xrdp/startwm.sh
-    echo "$password" | sudo -S sed -i 's|^\(exec\s/bin/sh.*\)|# \1|' /etc/xrdp/startwm.sh
-    grep "startxfce4" /etc/xrdp/startwm.sh
-    if [ $? != 0 ]; then
-        echo "$password" | sudo -S tee -a /etc/xrdp/startwm.sh <<< "startxfce4"
-    fi
-    if [ ! -e /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf ]; then
-        # cf. https://god-support.blogspot.com/2019/11/ubuntu1804-xrdp-authentication-is.html
-        echo "$password" | sudo -S curl -fsSLo /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf https://github.com/applejxd/dotfiles/raw/main/config/02-allow-colord.conf
-    fi
-
     # WSL config
     if [ ! -L /etc/wsl.conf ]; then
         echo "$password" | sudo -S rm /etc/wsl.conf
         echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/wsl.conf /etc/wsl.conf
     fi
-else
-    echo "$password" | sudo -S apt-get install -y chromium-browser  
 fi
+
+#######
+# SSH #
+#######
+
+echo "$password" | sudo -S apt-get purge openssh-server && apt-get install openssh-server
+if [ ! -L /etc/ssh/sshd_config ]; then
+    if [ ! -f /etc/ssh/sshd_config ]; then
+        echo "$password" | sudo -S rm /etc/ssh/sshd_config
+    fi
+    echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/sshd_config /etc/ssh/sshd_config
+fi
+
+#######
+# GUI #
+#######
+
+# RDP
+echo "$password" | sudo -S apt-get install -y xfce4 xrdp
+
+# RDP settings
+# cf. https://qiita.com/atomyah/items/887a5185ec9a8206c7c4#ubuntu%E3%81%ABxrdp%E3%82%92%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB
+
+# xrdp.ini configuration
+echo "$password" | sudo -S bash -c "\
+    sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini && \
+    sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini && \
+    sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini"
+echo xfce4-session > ~/.xsession
+
+# startwm.sh configuration
+echo "$password" | sudo -S sed -i 's|^\(test\s-x\s/etc/X11/Xsession.*\)|# \1|' /etc/xrdp/startwm.sh
+echo "$password" | sudo -S sed -i 's|^\(exec\s/bin/sh.*\)|# \1|' /etc/xrdp/startwm.sh
+if (! grep "startxfce4" /etc/xrdp/startwm.sh >/dev/null 2>&1); then
+    echo "$password" | sudo -S tee -a /etc/xrdp/startwm.sh <<< "startxfce4"
+fi
+
+# to fix Authentication error
+# cf. https://god-support.blogspot.com/2019/11/ubuntu1804-xrdp-authentication-is.html
+if [ ! -L /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf ]; then
+        if [ ! -f /etc/ssh/sshd_config ]; then
+        echo "$password" | sudo -S rm /etc/ssh/sshd_config
+    fi
+    echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/02-allow-colord.conf /etc/polkit-1/localauthority.conf.d/02-allow-colord.conf
+fi
+   
+echo "$password" | sudo -S apt-get install -y chromium-browser  
