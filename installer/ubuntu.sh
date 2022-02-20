5fd19a109d7fce039c007d986a9a1dd05fbabd66
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-02-20 14:27:47 +0900

    Update ubuntu.sh

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -10,7 +10,7 @@ fi
 # Refresh
 echo "$password" | sudo -S apt-get -y update && apt-get -y upgrade
 
-if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]] && (systemctl >/dev/null 2>&1) ; then
+if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]] && (systemctl >/dev/null 2>&1); then
     # systemctl
     curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
     echo "$password" | sudo -S bash -c "\
