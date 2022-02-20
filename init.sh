ead2582609576c096265da14f41875664a637c70
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-02-20 13:39:12 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,44 +7,6 @@ else
     password=$1
 fi
 
-################
-# Requirements #
-################
-
-# for ruby-build in Ubuntu (cf. http://tinyurl.com/j6g67up)
-if [[ -e /etc/lsb-release ]] && !(type "ruby-build" > /dev/null 2>&1); then
-    # To prevent stopping at interactive mode (needs -E option for sudo)
-    export DEBIAN_FRONTEND=noninteractive
-    echo "$password" | sudo -S bash -c "\
-        apt-get update && apt-get upgrade -y && \
-        apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev libdb-dev"
-fi
-
-if [[ "$OSTYPE" == "darwin"* ]] && !(type "brew" > /dev/null 2>&1); then
-    # The altanative of process substitution for bash 3.2 that is installed to Mac OS X
-    source /dev/stdin <<<"$( curl -fsSL https://raw.githubusercontent.com/applejxd/dotfiles/main/installer/homebrew.sh )"
-    # enable brew command
-    # eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    if [[ $(uname -m) == arm64 ]]; then
-        export ANYENV_ROOT=~/.anyenv_arm64
-    elif [[ $(uname -m) == x86_64 ]]; then
-        export ANYENV_ROOT=~/.anyenv_x64
-    fi
-else
-    export ANYENV_ROOT=~/.anyenv
-fi
-export PATH=$ANYENV_ROOT/bin:$PATH
-
-if !(type "anyenv" > /dev/null 2>&1); then
-    # install anyenv
-    git clone https://github.com/anyenv/anyenv $ANYENV_ROOT
-    anyenv init
-    yes | anyenv install --init
-fi
-
 ###################
 # OS dependencies #
 ###################
