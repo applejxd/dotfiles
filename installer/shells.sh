faec9d0719655e7b75f18438c9f245709ab83959
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-02-23 14:05:07 +0900

    Update shells.sh

--- installer/shells.sh
+++ installer/shells.sh
@@ -8,25 +8,29 @@ else
     password=$1
 fi
 
-# bash install
+###########
+# Install #
+###########
+
+# zsh install or update
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "$password" | sudo -S apt-get install -y zsh fish
+fi
+
+############
+# Register #
+############
+
+# register bash
 if ! grep -q $(which bash) /etc/shells; then
     echo "$password" | sudo -S sh -c "echo $(which bash) >> /etc/shells"
 fi
 
-# # fish install
-# if type "fish" > /dev/null 2>&1; then
-#     if ! grep -q $(which fish) /etc/shells; then
-#         echo "$password" | sudo -S sh -c "echo $(which fish) >> /etc/shells"
-#     fi
-# fi
-
-#######
-# zsh #
-#######
-
-# zsh install or update
-if (! type "zsh" > /dev/null 2>&1) && [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "$password" | sudo -S apt-get install -y zsh
+# register fish
+if type "fish" > /dev/null 2>&1; then
+    if ! grep -q $(which fish) /etc/shells; then
+        echo "$password" | sudo -S sh -c "echo $(which fish) >> /etc/shells"
+    fi
 fi
 
 # register zsh
@@ -35,6 +39,10 @@ if type "zsh" > /dev/null 2>&1; then
         echo "$password" | sudo -S sh -c "echo $(which zsh) >> /etc/shells"
     fi
 fi
+
+############
+# Register #
+############
 
 # if [[ "$OSTYPE" != "darwin"* ]]; then
     # if [[ $SHELL != $(which zsh) ]]; then
