9bafc2fe14c5d9f9c53c73b8cdede751bd774794
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-03-07 20:14:39 +0900

    Update homebrew.sh

--- installer/homebrew.sh
+++ installer/homebrew.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+    # save password
+    read -sp "Password: " password
+else
+    password=$1
+fi
+
 # Requirements
 if [[ "$OSTYPE" == "darwin"* ]]; then
     xcode-select --install
 elif [[ -e /etc/lsb-release ]]; then
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    # for Homebrew (cf. http://tinyurl.com/y5yh2vm3)
-    sudo apt-get install -y build-essential curl file git
+    echo "$password" | sudo -S apt-get update -y
+    echo "$password" | sudo -S apt-get upgrade -y
+    # for Homebrew
+    # cf. https://docs.brew.sh/Homebrew-on-Linux
+    echo "$password" | sudo -S apt-get install -y build-essential curl file git
 fi
 
 # Install Homebrew for Mac OS X or Linux
@@ -17,7 +25,7 @@ fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    # cf. http://tinyurl.com/y5yh2vm3
+    # cf. https://docs.brew.sh/Homebrew-on-Linux
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     test -r ~/.bashrc && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
