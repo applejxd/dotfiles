c66cd86a9eac2ea396d31e1f552368117d0794ac
applejxd <9639592+applejxd@users.noreply.github.com>
2022-03-27 22:02:13 +0900

    fix bug

--- installer/docker.sh
+++ installer/docker.sh
@@ -24,7 +24,7 @@ if !(type "docker" > /dev/null 2>&1); then
     # register GPG key of Docker official
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     # add stable repository
-    { echo "$password"; echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" } \
+    { echo "$password"; echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"; } \
     | sudo -k -S tee /etc/apt/sources.list.d/docker.list &>/dev/null
     
     echo "$password" | sudo -S apt-get update
