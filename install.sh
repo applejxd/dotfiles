5a004c4172565bacb6b83116e558a3c5f8b53d42
applejxd <9639592+applejxd@users.noreply.github.com>
2022-03-31 02:26:28 +0900

    update

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 if [ $# -eq 0 ]; then
     # save password
-    read -sp "Password: " password
+    read -rsp "Password: " password
 else
     password=$1
 fi
 
-echo $password | source <(curl -fsSL https://raw.githubusercontent.com/applejxd/dotfiles/main/deploy.sh)
-echo $password | source <(curl -fsSL https://raw.githubusercontent.com/applejxd/dotfiles/main/init.sh)
+echo "$password" | source <(curl -fsSL https://raw.githubusercontent.com/applejxd/dotfiles/main/deploy.sh)
+echo "$password" | source <(curl -fsSL https://raw.githubusercontent.com/applejxd/dotfiles/main/init.sh)
