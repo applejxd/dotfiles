1964443c8866e84e7e1cc6ce12590dcf7911aa96
りんごくん <applejxd@users.noreply.github.com>
2022-04-01 12:18:04 +0900

    update

--- installer/ubuntu/cuda.sh
+++ installer/ubuntu/cuda.sh
@@ -7,42 +7,48 @@ else
     password=$1
 fi
 
+########
+# CUDA #
+########
+
 # Control Driver and CUDA Toolkit separatelly
 # cf. https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html#ubuntu-lts
 # cf. https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#package-manager-metas
 
-distribution=$(. /etc/os-release; echo "$ID""$VERSION_ID" | sed -e 's/\.//g')
+if [[ ! -e /usr/local/cuda-11.6 ]];then
+    distribution=$(. /etc/os-release; echo "$ID""$VERSION_ID" | sed -e 's/\.//g')
 
-if [[ ! "$(uname -r)" =~ (M|m)icrosoft ]]; then
-    # Step 1: The kernel headers and dev packages
-    echo "$password" | sudo -S apt-get -y install linux-headers-"$(uname -r)"
-    # Step 2: Use the CUDA network repository
-    wget https://developer.download.nvidia.com/compute/cuda/repos/"$distribution"/x86_64/cuda-"$distribution".pin
-    echo "$password" | sudo -S mv cuda-"$distribution".pin /etc/apt/preferences.d/cuda-repository-pin-600
-fi
+    if [[ ! "$(uname -r)" =~ (M|m)icrosoft ]]; then
+        # Step 1: The kernel headers and dev packages
+        echo "$password" | sudo -S apt-get -y install linux-headers-"$(uname -r)"
+        # Step 2: Use the CUDA network repository
+        wget https://developer.download.nvidia.com/compute/cuda/repos/"$distribution"/x86_64/cuda-"$distribution".pin
+        echo "$password" | sudo -S mv cuda-"$distribution".pin /etc/apt/preferences.d/cuda-repository-pin-600
+    fi
 
-if  (! dpkg -l cuda-drivers > /dev/null 2>&1); then
-    # Step 3: The CUDA network repository public GPG key
-    echo "$password" | sudo -S apt-key adv --fetch-keys \
-    https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+    if  (! dpkg -l cuda-drivers > /dev/null 2>&1); then
+        # Step 3: The CUDA network repository public GPG key
+        echo "$password" | sudo -S apt-key adv --fetch-keys \
+        https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 
-    # Step 4: Setup the CUDA network repository
-    # cf. https://unix.stackexchange.com/questions/391796/pipe-password-to-sudo-and-other-data-to-sudoed-command
-    { echo "$password"; echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /"; } \
-    | sudo -k -S tee /etc/apt/sources.list.d/cuda.list &>/dev/null
+        # Step 4: Setup the CUDA network repository
+        # cf. https://unix.stackexchange.com/questions/391796/pipe-password-to-sudo-and-other-data-to-sudoed-command
+        { echo "$password"; echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /"; } \
+        | sudo -k -S tee /etc/apt/sources.list.d/cuda.list &>/dev/null
 
-    # Step 5: Update and installation
-    echo "$password" | sudo -S apt update
-fi
+        # Step 5: Update and installation
+        echo "$password" | sudo -S apt update
+    fi
 
-if [[ ! "$(uname -r)" =~ (M|m)icrosoft ]]; then
-    # Do not install any Linux display driver in WSL
-    # cf. https://docs.nvidia.com/cuda/wsl-user-guide/index.html#ch02-getting-started
-    echo "$password" | sudo -S apt-get install -y cuda-drivers
-fi
+    if [[ ! "$(uname -r)" =~ (M|m)icrosoft ]]; then
+        # Do not install any Linux display driver in WSL
+        # cf. https://docs.nvidia.com/cuda/wsl-user-guide/index.html#ch02-getting-started
+        echo "$password" | sudo -S apt-get install -y cuda-drivers
+    fi
 
-echo "$password" | sudo -S apt-get -y install cuda-toolkit-11-6
-export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}
+    echo "$password" | sudo -S apt-get -y install cuda-toolkit-11-6
+    export PATH=/usr/local/cuda-11.6/bin${PATH:+:${PATH}}
+fi 
 
 #################
 # Nvidia Docker #
