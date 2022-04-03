ab2dcb3ab4a687d2787e7dba74cf1c7119398179
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-04-03 10:38:24 +0900

    Update brew_mas_cask.rb

--- installer/osx/brew_mas_cask.rb
+++ installer/osx/brew_mas_cask.rb
@@ -1,3 +1,4 @@
+# Fucntion to check whether current architechture is arm64 or not
 # cf. https://zenn.dev/mactkg/articles/71d09e350315f9
 def is_m1?
   !RUBY_PLATFORM.index("arm64e").nil?
@@ -21,33 +22,8 @@ mas "GoodNotes 5", id: 1444383602
 # Cask for Mac App
 tap "homebrew/cask"
 
-#--------------#
-# Developments #
-#--------------#
-
-# GUI Apps
+# Homebrew GUI manager
 cask "cakebrew"
-# cask "sourcetree"
-
-# Editor and IDE
-cask "visual-studio-code"
-cask "jetbrains-toolbox"
-cask "emacs"
-
-# The shell emulator iTerm2
-cask "iterm2"
-
-# Fonts
-tap "homebrew/cask-fonts"
-cask "font-hack-nerd-font"
-
-# Emulation
-cask "docker"
-# cask "virtualbox"
-# cask "virtualbox-extension-pack"
-
-# Reference
-cask "dash"
 
 #--------------#
 # Fundamentals #
@@ -57,17 +33,16 @@ cask "dash"
 #cask "avast-security"
 cask "google-chrome"
 cask "google-japanese-ime"
+cask "bitwarden"
+brew "bitwarden-cli"
 
-# Online storages
-# cask "dropbox"
-# cask "google-backup-and-sync"
-# cask "google-drive"
+# Utilities
+cask "iterm2"
+cask "alfred"
+cask "hyperswitch"
+cask "karabiner-elements"
 
-#------------------#
-# System Asistants #
-#------------------#
-
-# quicklook
+# QuickLook
 cask "qlcolorcode"
 cask "qlstephen"
 cask "qlmarkdown"
@@ -78,59 +53,39 @@ cask "suspicious-package"
 cask "quicklookase"
 cask "qlvideo"
 
-# system
-cask "alfred"
-cask "hyperswitch"
-cask "karabiner-elements"
+#--------------#
+# Developments #
+#--------------#
 
-# Drive format support
-cask "osxfuse"
+# Editor and IDE
+cask "visual-studio-code"
+cask "jetbrains-toolbox"
 
-# Remote directory mounting
-# brew "sshfs" unless is_m1?
+# Fonts
+tap "homebrew/cask-fonts"
+cask "font-hack-nerd-font"
+
+# Environments
+cask "xquartz"
+cask "docker"
+
+# Reference
+cask "dash"
 
 #----------#
 # Mac Apps #
 #----------#
 
-# utilities
-cask "bitwarden"
-brew "bitwarden-cli"
-cask "discord"
-# cask "skype"
-# cask "evernote"
-
 # media
 cask "spotify"
 cask "kindle"
+cask "discord"
 
 # file manager
 cask "cyberduck"
 cask "omnidisksweeper"
-
-# graph
-# cask "xmind"
-# cask "drawio"
+#cask "sourcetree"
 
 # Research Tools
-# cask "mendeley"
-# cask "mathpix-snipping-tool"
-
-#-----------#
-# Platforms #
-#-----------#
-
-# cask "mactex"
-# cask "mamp"
-# cask "julia"
-# cask "r"
-# cask "sage"
-
-#--------#
-# Others #
-#------=-#
-
-# rogue-like game 'jnethack'
-cask "xquartz"
-brew "jnethack"
-brew "cocot"
+#cask "mendeley"
+#cask "mathpix-snipping-tool"
