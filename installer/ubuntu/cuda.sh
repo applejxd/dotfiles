e851079f92d2100145329f230eff804df81ad481
applejxd <9639592+applejxd@users.noreply.github.com>
2022-11-10 06:24:28 +0900

    update

--- installer/ubuntu/cuda.sh
+++ installer/ubuntu/cuda.sh
@@ -62,6 +62,16 @@ if [[ ! -e /usr/local/"$cuda_major_version"."$cuda_minor_version" ]];then
     # Modulefile
     echo "$password" | sudo -S mkdir -p /usr/local/Modules/modulefiles/cuda
     echo "$password" | sudo -S ln -s "$HOME"/src/dotfiles/config/cuda-11.2.module /usr/local/Modules/modulefiles/cuda/11.2
+    # whether the initial loadings are commented
+    module_init_file="/usr/local/Modules/etc/initrc"
+    if [[ $(grep "^.*\[is-saved default\].*" $module_init_file) =~ ^#.* ]]; then
+        line_num=$(grep -n "^.*\[is-saved default\].*" $module_init_file | awk -F ':' '{print $1}')
+        echo "$password" | sudo -S sed -i -e "$line_num,$((line_num+4)) s/#//g" $module_init_file
+    fi
+    # append initial loading
+    if [[ ! $(grep "^\s*module load.*" $module_init_file) =~ ^.*cuda/$cuda_major_version\.$cuda_minor_version.* ]]; then
+        echo "$password" | sudo -S sed -i -e "s/\(^\s*module load.*\)/\1 cuda\/$cuda_major_version\.$cuda_minor_version/g" $module_init_file
+    fi
 fi 
 
 #################
