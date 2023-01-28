ab288c2c89fa9ada608f89459a97480d8662eff7
applejxd <9639592+applejxd@users.noreply.github.com>
2023-01-29 08:23:15 +0900

    update

--- installer/ubuntu/cpp.sh
+++ installer/ubuntu/cpp.sh
@@ -7,6 +7,9 @@ else
     password=$1
 fi
 
+echo "$password" | sudo -S apt-get update
+echo "$password" | sudo -S apt-get -y upgrade
+
 function cmake_install() {
   INSTALL_PATH=$(echo "$1" | sed -e 's/^https\:\/\/\(.*\)\.git$/\1/g')
   if [ ! -e ~/src/"$INSTALL_PATH"/build ]; then
@@ -14,28 +17,61 @@ function cmake_install() {
     mkdir ~/src/"$INSTALL_PATH"/build
     cd ~/src/"$INSTALL_PATH"/build || exit
     cmake ..
-    make -j4
+    make -j"$(nproc)"
     echo "$password" | sudo -S make install
   fi
 }
 
+#-------#
+# cmake #
+#-------#
+
+# older version
+# echo "$password" | sudo -S apt-get install -y cmake
+
+# libncurses5-dev for ccmake
+# cf. https://stackoverflow.com/questions/28110169/update-ccmake-on-ubuntu-when-building-from-source
+echo "$password" | sudo -S apt-get install -y git build-essential libssl-dev libncurses5-dev
+mkdir -p "$HOME"/src/install
+# Install CMake 3.21.6 (for g2o)
+# CLion supports CMake 2.8.11~3.21.x
+git clone https://gitlab.kitware.com/cmake/cmake.git -b v3.21.6 "$HOME"/src/install/cmake
+mkdir "$HOME"/src/install/cmake/build; cd "$_" || exit
+../bootstrap && make -j"$(nproc)"
+echo "$password" | sudo -S make install
+cd "$HOME" || exit
+
+
+#-------#
+# Basic #
+#-------#
+
 # C/C++ Build Tools
-echo "$password" | sudo -S apt-get install -y cmake cmake-curses-gui gcc clang gdb build-essential
+echo "$password" | sudo -S apt-get install -y cmake-curses-gui gcc clang gdb
 
 # Formatter, Linter, Document Generator
 echo "$password" | sudo -S apt-get install -y clang-format cpplint doxygen graphviz
 
-# Google OSS
+#--------#
+# Google #
+#--------#
+
+# gflags
 echo "$password" | sudo -S apt-get install -y  libgflags-dev
+
+# gtest
 cmake_install https://github.com/google/googletest.git
 
-wget -P ~/src https://github.com/google/glog/archive/refs/tags/v0.5.0.zip
-unzip ~/src/v0.5.0.zip
-mkdir ~/src/glog-0.5.0/build
-cd ~/src/glog-0.5.0/build || exit
-cmake ..
-make -j4
+# glog
+mkdir -p "$HOME"/src/install
+git clone https://github.com/google/glog.git -b v0.5.0 "$HOME"/src/install/glog
+mkdir "$HOME"/src/install/glog/build; cd "$_" || exit
+cmake .. && make -j"$(nproc)"
 echo "$password" | sudo -S make install
+
+#------#
+# Libs #
+#------#
 
 # Boost, Eigen3 (Linear Algebra), Ceres (Optimization)
 echo "$password" | sudo -S apt-get install -y libboost-dev libeigen3-dev libceres-dev
