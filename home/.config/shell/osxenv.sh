4d33defa08b52374fad9add232590d1b7f1671f0
applejxd <9639592+applejxd@users.noreply.github.com>
2023-02-05 20:12:46 +0900

    update

--- home/.config/shell/osxenv.sh
+++ home/.config/shell/osxenv.sh
@@ -1,8 +1,8 @@
 export LANG=ja_JP.UTF-8
 
-########
+#------#
 # PATH #
-########
+#------#
 
 # The default
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin${PATH:+:${PATH}}
@@ -15,9 +15,32 @@ if [[ -e "/opt/homebrew/opt/openssl@1.1" ]]; then
   export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 fi
 
-#################
+#------------#
+# BSD -> GNU # 
+#------------#
+
+# coreutils
+PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
+# ed
+PATH=/usr/local/opt/ed/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/ed/libexec/gnuman:$MANPATH
+# findutils
+PATH=/usr/local/opt/findutils/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/findutils/libexec/gnuman:$MANPATH
+# sed
+PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH
+# tar
+PATH=/usr/local/opt/gnu-tar/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/gnu-tar/libexec/gnuman:$MANPATH
+# grep
+PATH=/usr/local/opt/grep/libexec/gnubin:$PATH
+MANPATH=/usr/local/opt/grep/libexec/gnuman:$MANPATH
+
+#---------------#
 # Specific root #
-#################
+#---------------#
 
 # for Maxima
 if [[ -e /Applications/wxMaxima.app/ ]]; then
