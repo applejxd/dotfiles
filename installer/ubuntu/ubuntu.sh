4d67f2b22ed31147305b13ee064694453f872bd0
りんごくん <9639592+applejxd@users.noreply.github.com>
2024-05-13 23:17:00 +0900

    Update ubuntu.sh

--- installer/ubuntu/ubuntu.sh
+++ installer/ubuntu/ubuntu.sh
@@ -37,7 +37,10 @@ if ! (type "code" >/dev/null 2>&1); then
     echo "$password" | sudo -S  apt-get install -y wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     echo "$password" | sudo -S  install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    {
+        echo "$password"
+        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main"
+    } | sudo -k -S tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
 
     echo "$password" | sudo -S apt-get install -y apt-transport-https
