ef3e5d94a2aea87760eca8cedda703c3461a70a2
りんごくん <9639592+applejxd@users.noreply.github.com>
2025-03-23 23:11:43 +0900

    Update ssh.sh

--- installer/ubuntu/ssh.sh
+++ installer/ubuntu/ssh.sh
@@ -7,16 +7,17 @@ else
     password=$1
 fi
 
-# Refresh
-echo "$password" | sudo -S apt-get -y update && apt-get -y upgrade
-
-echo "$password" | sudo -S bash -c "\
-    apt-get purge -y openssh-server && \
-    apt-get install -y openssh-server"
-
+# change port number not to conflict host sshd server for WSL
 if [ ! -L /etc/ssh/sshd_config ]; then
     if [ -f /etc/ssh/sshd_config ]; then
         echo "$password" | sudo -S mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
     fi
     echo "$password" | sudo -S ln -s ~/.homesick/repos/dotfiles/config/sshd_config /etc/ssh/sshd_config
 fi
+
+# Refresh
+echo "$password" | sudo -S apt-get -y update && apt-get -y upgrade
+
+echo "$password" | sudo -S bash -c "\
+    apt-get purge -y openssh-server && \
+    apt-get install -y openssh-server"
