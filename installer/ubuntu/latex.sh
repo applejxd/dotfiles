df2803b5a8d3dba0f70d5344db520182da6c3df0
applejxd <9639592+applejxd@users.noreply.github.com>
2025-03-25 00:10:32 +0900

    TeX Liveのフルインストールプロファイルを追加し、Ubuntuインストーラーを更新

--- installer/ubuntu/latex.sh
+++ installer/ubuntu/latex.sh
@@ -14,8 +14,10 @@ mkdir "${HOME}/instalal-tl"
 echo "$password" | sudo -S mount -o loop /tmp/texlive.iso "${HOME}/instalal-tl"
 
 # install
+# see https://www.tug.org/texlive/doc/install-tl.html
+# see https://chatgpt.com/share/67e15e0c-9b40-8008-b3b5-d6d2f8dee813
 cd "${HOME}/instalal-tl" || exit
-echo "$password" | sudo -S ./install-tl
+./install-tl -profile "${HOME}/src/dotfiles/config/texlive.profile"
 
 # refresh
 cd "${HOME}" || exit
