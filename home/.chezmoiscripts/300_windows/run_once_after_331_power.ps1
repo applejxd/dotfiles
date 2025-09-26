26aec6bd70bf3e20871debfbec2da926df9852f9
applejxd <9639592+applejxd@users.noreply.github.com>
2025-09-27 08:40:20 +0900

    update for windows config files

---
+++ home/.chezmoiscripts/300_windows/run_once_after_331_power.ps1
@@ -0,0 +1,3 @@
+# カバーを閉じたらスリープ
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
+powercfg /setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
---
+++ home/AppData/Local/Microsoft/PowerToys/NewPlus/Template/README.md
@@ -0,0 +1,3 @@
+# References
+
+Learn more about New+ by visiting [this official site](https://aka.ms/PowerToysOverview_NewPlus)
