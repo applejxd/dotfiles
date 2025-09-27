f8e478af5f2bf7165c6305265e3764d9f9a29215
applejxd <9639592+applejxd@users.noreply.github.com>
2025-09-27 09:00:26 +0900

    update windows files

---
+++ home/.chezmoiscripts/300_windows/310_packages/run_once_before_312_choco.ps1
@@ -0,0 +1,65 @@
+<#
+  .SYNOPSIS
+    Install keypirinha and its extentions
+  .DESCRIPTION
+    Install keypirinha via chocolatey
+#>
+
+# Install chocolatey
+if (-not (Get-Command choco -ea SilentlyContinue)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+choco install chocolateygui
+choco install Keypirinha
+
+# ---------- #
+# Extensions #
+# ---------- #
+
+$install_dir = "$env:UserProfile\src\windows-setup\tools\Keypirinha\InstalledPackages"
+if (-not (Test-Path $install_dir)) {
+    New-Item $install_dir -ItemType Directory
+}
+
+# To avoid Internet Explorer initialization
+Function Invoke-UpdatedWebRequest {
+    $version = $PSVersionTable.PSVersion.Major
+    if ($version -le 5) {
+        $cmd = "Invoke-WebRequest $args -UseBasicParsing" 
+    }
+    else {
+        $cmd = "Invoke-WebRequest $args" 
+    }
+    Invoke-Expression $cmd
+}
+  
+# function for downloading Keypirinha extensions
+function InstallRelease($repo_name, $file_path) { 
+    if (-not (Test-Path $install_dir\$file_path)) {
+        # GitHub Release API
+        $uri = "https://api.github.com/repos/" + $repo_name + "/releases/latest"
+        # Read json
+        $json = Invoke-UpdatedWebRequest $uri | ConvertFrom-Json
+        # Get URL
+        $url = $json.assets.browser_download_url
+        # Download
+        Invoke-UpdatedWebRequest $url -OutFile $file_path
+    }
+}
+
+InstallRelease "Fuhrmann/keypirinha-url-shortener" "$install_dir\URLShortener.keypirinha-package"
+InstallRelease "psistorm/keypirinha-systemcommands" "$install_dir\SystemCommands.keypirinha-package"
+InstallRelease "clinden/keypirinha-colorpicker" "$install_dir\ColorPicker.keypirinha-package"
+InstallRelease "dozius/keypirinha-snippets" "$install_dir\Snippets.keypirinha-package"
+# Clipborad Manager
+InstallRelease "tuteken/Keypirinha-Plugin-Ditto" "$install_dir\Ditto.keypirinha-package"
+# Default Windows Apps
+InstallRelease "ueffel/Keypirinha-WindowsApps" "$install_dir\WindowsApps.keypirinha-package"
+# Windows Terminal Profiles
+InstallRelease "fran-f/keypirinha-terminal-profiles" "$install_dir\Terminal-Profiles.keypirinha-package"
+# Search by abbrev
+InstallRelease "bantya/Keypirinha-EasySearch" "$install_dir\EasySearch.keypirinha-package"
+# Execute commands from >
+InstallRelease "bantya/Keypirinha-Command" "$install_dir\Command.keypirinha-package"
---
+++ home/AppData/Roaming/Keyhac/config.py
@@ -0,0 +1,45 @@
+﻿"""
+Keyhac 設定ファイル
+See https://crftwr.github.io/keyhac/doc/ja/#id8
+"""
+
+import datetime
+import os
+import sys
+from pathlib import Path
+
+import pyauto
+from keyhac import *
+
+keyhac_path = Path(os.environ["USERPROFILE"]) / "AppData" / "Roaming" / "Keyhac"
+sys.path.append(str(keyhac_path))
+
+from extension import clipboard
+from extension.fakeymacs import config as fakeymacs
+
+
+def editor(path):
+    # VSCode があれば使用
+    editor_path = Path("C:\Program Files\Microsoft VS Code\Code.exe")
+    if not editor_path.exists():
+        # なければメモ帳
+        editor_path = Path("notepad.exe")
+    editor_path = f'\"{str(editor_path)}\"'
+        
+    # see https://crftwr.github.io/keyhac/doc/ja/group__pyauto.html#gaed9cb14419b28016c15906a08d92102f
+    shellExecute( None, editor_path, '"%s"'% path, "" )
+
+
+def configure(keymap):
+    # Emacs キーバインド
+    fakeymacs.configure(keymap)
+
+    # クリップボード拡張
+    clipboard.configure(keymap)
+    
+    # 設定ファイルを開くエディタ
+    keymap.editor = editor
+    # リストウィンドウを白いテーマで
+    keymap.setTheme("white")
+
+    
